918bf87d5d26b562eab0135a215787ef04cb8dd6
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-06-04 20:52:52 +0530

    update

---
+++ Routes/router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = new express.Router();
+const controllers = require("../controllers/userControllers");
+
+// Routes
+router.post("/user/register", controllers.userregister);
+router.post("/user/sendotp", controllers.userOtpSend);
+router.post("/user/login", controllers.userLogin);
+
+module.exports = router;
